13c1bff4103792915111414e982e82c316b76a6b
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-01-19 16:55:11 +0530

    stack functionality added

--- Program.cs
+++ Program.cs
@@ -7,8 +7,85 @@ class MyClass
     public static void Main(string[] args)
     {
         Display();
-        
+        StackFeatures();
     }
+
+    private static void StackFeatures()
+    {
+        while (true)
+        {
+            Console.WriteLine("Please enter choise from following. ");
+            Console.WriteLine("1 To push a number into a stack");
+            Console.WriteLine("2 To pop a number from a stack");
+            Console.WriteLine("3 To check if perticular number is in stack");
+            Console.WriteLine("4 To clear the stack");
+            Console.WriteLine("5 To quit");
+
+            int choise = -1;
+            try
+            {
+                choise = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            switch (choise)
+            {
+                case 1:
+                    Console.WriteLine("Please enter a number to push");
+                    string input = Console.ReadLine();
+                    int num = Int32.MaxValue;
+                    try
+                    {
+                        num = int.Parse(input);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                    numStack.Push(num);
+                    Display();
+                    break;
+                case 2:
+                    num = numStack.Pop();
+                    Console.WriteLine(num + " Popped.");
+                    Display();
+                    break;
+                case 3:
+                    Console.WriteLine("Please enter a number to check if it's in stack");
+                    input = Console.ReadLine();
+                    num = Int32.MaxValue;
+                    try
+                    {
+                        num = int.Parse(input);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                    if (numStack.Contains(num))
+                    {
+                        Console.WriteLine("Yes " + num + " is in stack");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No " + num + " is not in stack");
+                    }
+                    Display();
+                    break;
+                case 4:
+                    numStack.Clear();
+                    Display();
+                    break;
+                default:
+                    Console.WriteLine("Exiting");
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+
     public static void Display()
     {
         Console.WriteLine("Members in slack are: ");
